4d377662a0046fd754fdd8bd2ccadbea4c83e57f
Diego Veras <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>
2024-07-05 13:32:19 -0300

    Added backend project files for Mind Match

---
+++ backend/mind-match/src/main/java/com/mindmatch/MindMatchApplication.java
@@ -0,0 +1,13 @@
+package com.mindmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindMatchApplication.class, args);
+	}
+
+}
